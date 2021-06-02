ebddd7d35e429068bda8e69a5da7df18355593f2
zhi <1106010597@qq.com>
2021-06-02 10:47:19 +0800

    update

---
+++ dist/index.8f0dcdfe.js
@@ -0,0 +1,2 @@
+!function(){const o=dom.find("#test>.red")[0];dom.style(o,"color","red");const d=dom.find(".red");dom.each(d,(o=>console.log(o)))}();
+//# sourceMappingURL=index.8f0dcdfe.js.map
---
+++ dist/index.9ba232cf.js
@@ -0,0 +1,2 @@
+window.dom={find:function(e,t){return(t||document).querySelectorAll(e)},style:function(e,t,n){if(3===arguments.length)e.style[t]=n;else if(2===arguments.length){if("string"==typeof t)return e.style[t];if(t instanceof Object)for(let n in t)e.style[n]=t[n]}},each:function(e,t){for(let n=0;n<e.length;n++)t.call(null,e[n])}};
+//# sourceMappingURL=index.9ba232cf.js.map
